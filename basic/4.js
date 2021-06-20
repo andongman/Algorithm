d62d8062cf20eaec9a027a4fb356e61e3494a2ad
sanglok <evertree6031@gmail.com>
2021-06-20 13:51:58 +0900

    4번

---
+++ basic/4.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = 2.22;
+let c = 'p';
+let d = [1, 2, 3];
+
+console.log(toString.call(a));
+console.log(toString.call(b));
+console.log(toString.call(c));
+console.log(toString.call(d));
