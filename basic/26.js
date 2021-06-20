cd4bddc652804180e7f4fff92e4a7b9dd725bca9
sanglok <evertree6031@gmail.com>
2021-06-20 16:39:39 +0900

    26번

---
+++ basic/26.js
@@ -0,0 +1,13 @@
+const ary = [];
+ary["수성"] = "Mercury";
+ary["금성"] = "Venus";
+ary["지구"] = "Earth";
+ary["화성"] = "Mars";
+ary["목성"] = "Jupiter";
+ary["토성"] = "Saturn";
+ary["천왕성"] = "Uranus";
+ary["해왕성"] = "Neptune";
+
+let input = "토성";
+
+console.log(`${input}의 영어는 ${ary[input]}`);
