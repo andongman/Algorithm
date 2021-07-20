067ef7588604cdb19a4fd079f42740b3f4c06716
sanglok <evertree6031@gmail.com>
2021-07-20 19:17:14 +0900

    60번

---
+++ basic2/Q60/60.js
@@ -0,0 +1,8 @@
+function solution(students) {
+    students.sort().map((v, index) => console.log(`번호 : ${index + 1}, 이름 : ${v}`))
+}
+
+const students = ['강은지', '김유정', '박현서', '최성훈', '홍유진',
+    '박지호', '권윤일', '김채리', '한지호', '김진이', '김민호', '강채연'];
+
+solution(students);
