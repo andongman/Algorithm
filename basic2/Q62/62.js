5470de742d2857dcacd44075faad9386622bbf51
sanglok <evertree6031@gmail.com>
2021-07-21 23:29:13 +0900

    62번

---
+++ basic2/Q62/62.js
@@ -0,0 +1,7 @@
+function solution() {
+    let str = "aacdddddddddfffffffffgghhh";
+
+    console.log(`${str.match(/a/g).length}${Number(str.match(/b/g))}${str.match(/c/g).length}${str.match(/d/g).length}${Number(str.match(/e/g))}${str.match(/f/g).length}${str.match(/g/g).length}${str.match(/h/g).length}`)
+}
+
+solution();
