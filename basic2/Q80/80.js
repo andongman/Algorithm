3d30f7237a155c39ac9993ed0348c2a839a997d2
kimsanglok <evertree6031@gmail.com>
2021-08-10 14:37:58 +0900

    80번

---
+++ basic2/Q80/80.js
@@ -0,0 +1,21 @@
+function solution(str, num) {
+    const ary = str.split(" ");
+    let res = [];
+
+    function recursion(str, ary) {
+        for (let i = 0; i < ary.length; i++) {
+            res.push(str + ary[i]);
+            recursion(str + ary[i], ary.slice(i + 1));
+        }
+    }
+
+    recursion('', ary);
+
+    res = res.filter(v => v.length === num);
+
+    console.log(res);
+    console.log(res.length);
+}
+
+solution("ㄱ ㄴ ㄷ ㄹ", 3);
+
