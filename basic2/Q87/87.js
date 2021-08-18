0549f4daf8c71a37175be25feb9d8e7e4c1b4573
kimsanglok <evertree6031@gmail.com>
2021-08-18 13:41:33 +0900

    87번

---
+++ basic2/Q87/87.js
@@ -0,0 +1,28 @@
+function solution(names, dishes) {
+    let obj = {};
+
+    names.map((_, idx) => obj[names[idx]] = dishes[idx]);
+
+    let res = Object.entries(obj).sort((a, b) => b[1] - a[1]).map((v, idx) => {
+        let value = [...v];
+        value[1] = idx + 1;
+        return value;
+    });
+
+    return res;
+}
+
+//test
+const names = ["홍길동", "엄석대", "연개소문", "김첨지"];
+const dishes = [2, 1, 10, 0];
+
+//test
+// const names = ["손오공", "야모챠", "메지터", "비콜로"];
+// const dishes = [70, 10, 55, 40];
+
+console.log(solution(names, dishes));
+
+
+temp = [{ name: value }, { name: value } ...]
+let result = {};
+for (let i of temp) { result[i['name']] = i['value']; }
---
+++ basic2/Q87/README.md
@@ -0,0 +1,13 @@
+문제 푼 날짜 : 2021.8.18
+
+<h2>문제 : 천하제일 먹기 대회</h2>
+
+<h3>내가 푼 방식</h3>
+<div>1. 이름(key)과 먹은 접시(value)로 구성된 객체를 만든다.</div>
+<div>2. 객체를 배열로 바꾼 뒤, 내림차순 정렬과 map()을 사용해 value값을 바꿔서 해결.</div>
+
+<h4>새로 배운 점</h4>
+let temp = [{ name: value }, { name: value } ,...];
+let result = {};
+for (let i of temp) { result[i['name']] = i['value']; }
+<div>result[i['name']] 와 같이 사용할 수 있다.</div>
