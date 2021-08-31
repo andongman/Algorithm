c8203650980b6a85597fdb49d9fadd31fc33c169
kimsanglok <evertree6031@gmail.com>
2021-08-31 22:29:48 +0900

    2016년

--- programmers/level1/32.2016년/solution.js
+++ programmers/level1/32.2016년/solution.js
@@ -1,4 +1,11 @@
 function solution(a, b) {
-    var answer = '';
-    return answer;
+    let date = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let fromDay = 0;
+
+    for (let i = 0; i < a - 1; i++) {
+        fromDay += days[i];
+    }
+    let res = (fromDay + b) % 7 + 4;
+    return res > 6 ? date[res - 7] : date[res];
 }
