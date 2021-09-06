89335b7f779d5791d811b65cba7e8415a66ad825
kimsanglok <evertree6031@gmail.com>
2021-09-06 14:24:20 +0900

    신규 아이디 추천

--- programmers/level1/49.신규 아이디 추천/README.md
+++ programmers/level1/49.신규 아이디 추천/README.md
@@ -3,7 +3,12 @@
 <h2>문제 : 신규 아이디 추천</h2>
 
 <h3>내가 푼 방식</h3>
-<div>1. 간단한 로직이기에 생략.</div>
+<div>1. 정규표현식을 사용하지 않아서 문제 해결하는데 오래 걸렸다.</div>
+
+<h3>깨달은 점</h3>
+<div>1. JS에서 문자열은 immutable형이어서 특정 인덱스의 수정이 불가능하다.</div>
+<div>2. ("0123456789abcdefghijklmnopqrstuvwxyz-_.".includes(val)) -> 많은 조건문을 간소화해주는 좋은 아이디어인 것 같다.</div>
+<div>3. 정규표현식의 공부가 시급하다!!</div>
 
 <br>
 https://programmers.co.kr/learn/courses/30/lessons/72410
--- programmers/level1/49.신규 아이디 추천/solution.js
+++ programmers/level1/49.신규 아이디 추천/solution.js
@@ -1,15 +1,29 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase()
-        .replace(/[^a-z0-9-_.]/gi, '')
-        .replace(/[.]{2,}/gi, '.')
-        .replace(/^[.]|[.]$/gi, '');
-    if (answer === '') answer = 'a';
-    if (answer.length > 15) {
-        answer = answer.substring(0, 15);
-        answer = answer.replace(/[.]$/gi, '');
+    let answer = "";
+
+    for (let idx in new_id) {
+        let val = new_id[idx].toLowerCase();
+
+        if (val === "." && answer[answer.length - 1] === ".") continue;
+        if (!("0123456789abcdefghijklmnopqrstuvwxyz-_.".includes(val))) continue;
+
+        answer += val;
     }
-    while (answer.length < 3) {
-        answer += answer[answer.length - 1];
+
+    while (answer[0] === '.' || answer[answer.length - 1] === '.') {
+        if (answer[0] === '.') answer = answer.slice(1);
+        if (answer[answer.length - 1] === '.') answer = answer.slice(0, answer.length - 1);
     }
+
+    if (!answer.length) answer = "a";
+
+    answer = answer.slice(0, 15);
+
+    while (answer[answer.length - 1] === '.') {
+        if (answer[answer.length - 1] === '.') answer = answer.slice(0, answer.length - 1);
+    }
+
+    if (answer.length <= 2) answer = answer.padEnd(3, answer[answer.length - 1]);
+
     return answer;
 }
