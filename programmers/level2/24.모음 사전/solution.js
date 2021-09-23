5e65727ef5ccd054a921cc41f6c9712ef36b8e9c
kimsanglok <evertree6031@gmail.com>
2021-09-24 01:01:59 +0900

    모음 사전

--- programmers/level2/24.모음 사전/solution.js
+++ programmers/level2/24.모음 사전/solution.js
@@ -1,4 +1,24 @@
 function solution(word) {
-    var answer = 0;
-    return answer;
+    let ary = [1, 2, 3, 4, 5];
+    let answer = [];
+    for (let i = 0; i < ary.length; i++) {
+        answer.push(String(ary[i]));
+        for (let j = 0; j < ary.length; j++) {
+            answer.push(String(ary[i]) + String(ary[j]));
+            for (let k = 0; k < ary.length; k++) {
+                answer.push(String(ary[i]) + String(ary[j]) + String(ary[k]));
+                for (let l = 0; l < ary.length; l++) {
+                    answer.push(String(ary[i]) + String(ary[j]) + String(ary[k]) + String(ary[l]));
+                    for (let m = 0; m < ary.length; m++) {
+                        answer.push(String(ary[i]) + String(ary[j]) + String(ary[k]) + String(ary[l]) + String(ary[m]));
+                    }
+                }
+            }
+        }
+    }
+    const pattern = { A: 1, E: 2, I: 3, O: 4, U: 5 };
+    let result = "";
+    [...word].forEach(v => result += pattern[v]);
+
+    return answer.sort().indexOf(result) + 1
 }
