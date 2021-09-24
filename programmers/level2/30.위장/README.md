e69c7d811cb5c7cccfea9161f4c78c6f6cd9c126
kimsanglok <evertree6031@gmail.com>
2021-09-25 02:18:56 +0900

    위장

---
+++ programmers/level2/30.위장/README.md
@@ -0,0 +1,14 @@
+문제 푼 날짜 : 2021.9.25
+
+<h2>문제 : 위장</h2>
+
+<h3>내가 푼 방식</h3>
+<div>1. 간단한 로직이기에 풀이 생략.</div>
+
+
+<h3>알게된 점</h3>
+<div>1. obj[kind] = (obj[kind] || 0) + 1;</div>
+<div>2. 1번과 같이 구성하면, if-else문으로 구성할 필요가 없어진다.</div>
+
+<br>
+https://programmers.co.kr/learn/courses/30/lessons/42578
---
+++ programmers/level2/30.위장/solution.js
@@ -0,0 +1,17 @@
+function solution(clothes) {
+    let obj = {};
+
+    for (let i = 0; i < clothes.length; i++) {
+        let [name, kind] = clothes[i];
+
+        if (obj[kind]) obj[kind].push(name);
+        else obj[kind] = [name];
+    }
+    let answer = 1;
+    let ary = Object.values(obj);
+
+    for (let i = 0; i < ary.length; i++) {
+        answer *= ary[i].length + 1;
+    }
+    return answer - 1;
+}
