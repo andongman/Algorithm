e934b95c3e2a80af4dc2ce4da26a95cb8f714a0a
kimsanglok <evertree6031@gmail.com>
2021-09-29 17:48:38 +0900

    튜플

---
+++ programmers/level2/36.튜플/README.md
@@ -0,0 +1,13 @@
+문제 푼 날짜 : 2021.9.29
+
+<h2>문제 : 튜플</h2>
+
+<h3>내가 푼 방식</h3>
+<div>1. 간단한 로직이기에 풀이 생략.</div>
+
+<h3>더 좋은 방식</h3>
+<div>1. "},{"로 split를 하면 훨씬 더 코드가 간편해진다.</div>
+<div>2. best code 참조</div>
+
+<br>
+https://programmers.co.kr/learn/courses/30/lessons/64065
---
+++ programmers/level2/36.튜플/solution.js
@@ -0,0 +1,40 @@
+function solution(s) {
+    let replica = s.slice(1, s.length - 1).split("");
+    let ary = [];
+
+    while (replica.length) {
+        let leftBracket = replica.indexOf("{");
+        let rightBracket = replica.indexOf("}");
+
+        if (leftBracket === -1 || rightBracket === -1) break;
+
+        let unit = replica.splice(leftBracket, rightBracket - leftBracket + 1);
+        unit = unit.slice(1, unit.length - 1).join("").split(",").map(v => parseInt(v));
+
+        ary.push(unit);
+    }
+    ary.sort((a, b) => a.length - b.length);
+    let result = [];
+
+    ary.map(ele => {
+        ele.map(v => {
+            if (!(result.includes(v))) result.push(v);
+        })
+    })
+    return result;
+}
+
+//best code
+// const tupleFrom = (str) =>
+//     str.slice(2, -2).split('},{')
+//         .map((it) => toNumbers(it))
+//         .sort(accendingByLength)
+//         .reduce((acc, cur) =>
+//             [...acc, ...cur.filter((it) => !acc.includes(it))], []);
+
+// const toNumbers = (str) => str.split(',').map(it => Number(it));
+
+// const accendingByLength = (arr1, arr2) => arr1.length - arr2.length;
+
+// const solution = (s) => tupleFrom(s);
+
