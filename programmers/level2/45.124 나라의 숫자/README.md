399b1db23c97e848fb6668ee715e9ff1ecc04f2e
kimsanglok <evertree6031@gmail.com>
2021-10-03 19:33:02 +0900

    124 나라의 숫자

---
+++ programmers/level2/45.124 나라의 숫자/README.md
@@ -0,0 +1,10 @@
+문제 푼 날짜 : 2021.10.03
+
+<h2>문제 : 124 나라의 숫자</h2>
+
+<h3>내가 푼 방식</h3>
+<div>1. 규칙성만 찾게 되면, 간단한 문제이다.</div>
+
+
+<br>
+https://programmers.co.kr/learn/courses/30/lessons/12899
---
+++ programmers/level2/45.124 나라의 숫자/solution.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let rule = [4, 1, 2];
+    let answer = [];
+
+    while (true) {
+        let quotient = Math.floor(n / 3);
+        let remainder = n % 3;
+
+        answer.push(rule[remainder]);
+
+        if (remainder === 0) quotient -= 1;
+        if (quotient === 0) break;
+
+        n = quotient; x
+    }
+    return answer.reverse().join("");
+}
