3e418bba2edbb05102423ddde88410d6ec5bc453
kimsanglok <evertree6031@gmail.com>
2021-10-05 11:56:28 +0900

    최소직사각형

--- programmers/level1/56.최소직사각형/README.md
+++ programmers/level1/56.최소직사각형/README.md
@@ -6,5 +6,8 @@
 <div>1. 아이디어를 생각하지 못하여 오래 걸렸다.</div>
 <div>2. 가로, 세로에서 큰 값을 인덱스 0으로, 작은 값을 인덱스 1로 값을 다듬은 뒤, 각 인덱스에서 가장 큰 값을 찾아서 문제 해결.</div>
 
+<h3>더 나은 방식</h3>
+<div>1. good code 참조</div>
+<div>2. Array.reduce()를 잘 활용하자.</div>
 <br>
 https://programmers.co.kr/learn/courses/30/lessons/86491
--- programmers/level1/56.최소직사각형/solution.js
+++ programmers/level1/56.최소직사각형/solution.js
@@ -2,3 +2,9 @@ function solution(sizes) {
     sizes = sizes.map(v => v[0] > v[1] ? v : [v[1], v[0]]);
     return Math.max(...sizes.map(v => v[0])) * Math.max(...sizes.map(v => v[1]));
 }
+
+//good code
+// function solution(sizes) {
+    // const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0]);
+//     return hor * ver;
+// }
